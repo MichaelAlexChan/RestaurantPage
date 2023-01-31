9f58199159965b4c4d3037ce62c885b02770fcb7
Michael Chan <michaelalexanderchan@gmail.com>
2023-01-31 11:40:38 -0500

    Changed js to add more elements on Home page

--- src/home.js
+++ src/home.js
@@ -17,10 +17,23 @@ function homeSectionFill() {
   subHead.textContent = 'Taste the treasures of the ocean!';
   container.appendChild(subHead);
 
+  const description = document.createElement('div');
+  description.classList.add('box');
   const text = document.createElement('p');
   text.textContent = `Paradise Cove restaurant offers a selection of high-quality seafood dishes
   and other amazing fare! Let us take you on a beach getaway with our amazing appetizers, succulent seafood, enrapturing entrees, and delicious desserts!`
-  container.appendChild(text);
+  description.appendChild(text);
+  container.appendChild(description);
+  
+  const hours = document.createElement('div');
+  hours.classList.add('box');
+  const hoursHead = document.createElement('h3');
+  hoursHead.textContent = 'Hours';
+  const hoursText = document.createElement('p');
+  hoursText.innerHTML = 'Sunday 8-8PM<br />Monday 8-8PM<br />Tuesday CLOSED<br />Wednesday 8-8PM<br />Thursday 8-8PM<br />Friday 8-8PM<br />Saturday 9-10PM'
+  hours.appendChild(hoursHead);
+  hours.appendChild(hoursText);
+  container.appendChild(hours);
 }
 
 export default homeSectionFill;
